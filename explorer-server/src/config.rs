60f29ae4134597904bac7b1caf1c54dfee627936
Adrian Dinis <adriandinis99@gmail.com>
2021-09-24 19:59:28 +0100

    Create module to manage configs

---
+++ explorer-server/src/config.rs
@@ -0,0 +1,21 @@
+
+use anyhow::{Result};
+use serde::{Deserialize};
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Modes {
+    Production,
+    Development,
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub mode: Modes,
+    pub index_database: String,
+}
+
+pub fn load_config(config_string: &str) -> Result<Config> {
+    let config: Config = toml::from_str(config_string).unwrap();
+    return Ok(config);
+}
